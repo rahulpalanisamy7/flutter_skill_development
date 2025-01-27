d7a7ce4ee2bb0faf67767a06b40f5846be73cae0
rahulpalanisamy7 <132165457+rahulpalanisamy7@users.noreply.github.com>
2025-01-27 12:27:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_skill_development
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_skill_development/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_skill_development
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
