ed60103c40131b7a395f35d4b89d16f0ad7dc7f4
rahulpalanisamy7 <rahulmass7703@gmail.com>
2025-02-07 20:17:51 +0530

    SplashScreen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_skill_development/screens/HomeScreen.dart';
+import 'package:flutter_skill_development/screens/SplashScreen.dart';
 import 'package:flutter_skill_development/screens/TopicScreen.dart';
 
 void main() async
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
        debugShowCheckedModeBanner: false,
       // home: HomeScreen(title: 'Sign Language Converter'),
-      home: TopicScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_skill_development/screens/TopicScreen.dart';
+import 'dart:async';
+
+import 'HomeScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => TopicScreen()),
+      ); // Navigate to HomeScreen
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/logo.jpg', width: 150), // Your splash image
+      ),
+    );
+  }
+}
