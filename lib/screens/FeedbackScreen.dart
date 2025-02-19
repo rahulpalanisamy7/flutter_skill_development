49773f72ba03645a3c3877454558a9e20baa3c53
rahulpalanisamy7 <rahulmass7703@gmail.com>
2025-02-19 20:09:46 +0530

    Update FeedbackScreen.dart

--- lib/screens/FeedbackScreen.dart
+++ lib/screens/FeedbackScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
 class FeedbackScreen extends StatefulWidget {
 
   final String title;
@@ -11,15 +14,82 @@ class FeedbackScreen extends StatefulWidget {
 }
 
 class _FeedbackScreenState extends State<FeedbackScreen> {
+  // final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _feedbackController = TextEditingController();
+
+  Future<void> _submitFeedback() async {
+    final feedback = _feedbackController.text;
+
+    if (feedback.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Feedback cannot be empty')),
+      );
+      return;
+    }
+
+    try {
+      final response = await Supabase.instance.client
+          .from('feedback') // Replace with your table name
+          .insert({
+        'title': widget.title,
+        'feedback': feedback,
+        // 'submitted_at': DateTime.now().toIso8601String(),
+      });
+
+      print(response);
+
+      if (response == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Feedback submitted successfully')),
+        );
+        _feedbackController.clear();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to submit feedback.')),
+        );
+      }
+    } catch (error) {
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(content: Text('Failed to submit feedback: $error')),
+      // );
+      // print(error);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Feedback'),
       ),
-      body: Center(
-        child: Text(widget.title),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Your Feedback',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _feedbackController,
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: 'Type your feedback here...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _submitFeedback,
+              child: Text('Submit'),
+            ),
+          ],
+        ),
       ),
     );
   }
